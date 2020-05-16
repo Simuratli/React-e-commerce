e0470493b628fa220214b2a7ad0413672b2e6a6c
Eljan Simuratli <44237641+Simuratli@users.noreply.github.com>
2020-05-16 01:51:33 -0700

    Updated menu-item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import Homepage from './homepage.component'
+import Homepage from './pages/homepage/homepage.component';
+import {Switch, Route} from "react-router-dom";
+import './index.css'
+
+const Hatpage = ()=>{
+  return (<div><h1>Hat page</h1></div>)
+}
+
+
 
 function App() {
   return (
     <div>
-      <Homepage />
+    <Switch>
+      <Route exact={true} path='/' component={Homepage} />
+      <Route exact={true} path='/hats' component={Hatpage} />
+    </Switch>
+ 
     </div>
   );
 }
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './menu-item.style.scss';
+import {withRouter} from 'react-router-dom'
+
+function MenuItem({title,imageUrl,size,history,linkUrl,match}) {
+    return (
+        <div  className={`${size} menu-item`} onClick={()=>{history.push(`${match.url}${linkUrl}`)}}>
+        <div className='background-image' style={{backgroundImage:`url(${imageUrl})`}} />
+        <div className='content'>
+          <div className='title'>{title.toUpperCase()}</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+    )
+}
+
+export default withRouter(MenuItem);
