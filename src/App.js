82f526a3c6fcbd4edf48bb3dd6c46b12692ed3f5
Eljan Simuratli <44237641+Simuratli@users.noreply.github.com>
2020-06-05 02:32:10 -0700

    firebase

--- src/App.js
+++ src/App.js
@@ -9,26 +9,40 @@ import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
   
-  constructor(){
+  constructor() {
     super();
+
     this.state = {
-      currentUser:null
+      currentUser: null
     };
-   
   }
 
   unsubscribeFromAuth = null;
 
-  componentDidMount(){
-   this.unsubscribeFromAuth =  auth.onAuthStateChanged(async user =>{
-    createUserProfileDocument(user)
-    })
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-
 
   render(){
     return (
